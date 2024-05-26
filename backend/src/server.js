aff05e6a4af542ea9822b3c2af0cdbafbe6146a4
lucio.fmmelo <lucio.fmmelo88@gmail.com>
2024-05-25 23:36:22 -0300

    Implement a basic backend using Node.js with WebSocket support

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const { WebSocketServer } = require("ws");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080  })
+
+wss.on('connection', (ws) => {
+    ws.on('error', (err) => console.log(err))
+
+    ws.on('mensage', (data) => {
+        
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log('client connected')
+})
